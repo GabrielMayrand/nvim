e88bb7780982571e1e7ffe4f0d9914a6e74a8b51
GabrielMayrand <gabmayrand@hotmail.ca>
2025-01-01 22:45:20 -0500

    bug: added fzf not working

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,15 +4,37 @@ return {
     tag = '0.1.8',
     dependencies = {
       'nvim-lua/plenary.nvim',
-      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+      {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        build = 'make',
+        config = function()
+          require("telescope").load_extension("fzf")
+        end,
+      }
     },
     config = function()
+      require('telescope').setup {
+        extensions = {
+          fzf = {
+            fuzzy = true,                   -- false will only do exact matching
+            override_generic_sorter = true, -- override the generic sorter
+            override_file_sorter = true,    -- override the file sorter
+            case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
+            -- the default case_mode is "smart_case"
+          }
+        }
+      }
+      -- To get fzf loaded and working with telescope, you need to call
+      -- load_extension, somewhere after setup function:
+      require('telescope').load_extension('fzf')
+
       vim.keymap.set('n', '<leader>ft', '<cmd>Telescope builtin<CR>')
 
       local builtin = require('telescope.builtin')
       vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
       vim.keymap.set('n', '<leader>fw', builtin.live_grep, { desc = 'Telescope live grep' })
       vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+      vim.keymap.set('n', '<leader>fq', builtin.quickfix, { desc = 'Telescope quickfix' })
 
       -- GIT
       vim.keymap.set('n', '<leader>gs', builtin.git_status, { desc = 'Telescope git status' })
