4c31c09e657d5a9d9408768feb63a8bf1d2046ed
GabrielMayrand <gabmayrand@hotmail.ca>
2025-01-15 19:19:12 -0500

    feat: nvim dap

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -7,6 +7,16 @@ return {
       "antoinemadec/FixCursorHold.nvim",
       "nvim-treesitter/nvim-treesitter",
       "Issafalcon/neotest-dotnet",
+      "mfussenegger/nvim-dap",
+    },
+    opts = {
+      discovery = {
+        enabled = false,
+        concurrent = 1,
+      },
+      running = {
+        concurrent = true,
+      },
     },
     config = function()
       require("neotest").setup({
@@ -19,8 +29,11 @@ return {
         },
       })
       vim.keymap.set('n', '<leader>tt', "<cmd>Neotest run<cr>", { desc = 'Neotest nearest test' })
-      vim.keymap.set('n', '<leader>tf', "<cmd>Neotest run file<cr>", { desc = 'Neotest test file' })
-      vim.keymap.set('n', '<leader>ts', "<cmd>require('neotest').run.stop()<cr>", { desc = 'Neotest test stop' })
+      vim.keymap.set('n', '<leader>ts', "<cmd>lua require('neotest').run.stop()<cr>", { desc = 'Neotest test stop' })
+      vim.keymap.set('n', '<leader>tf', "<cmd>lua require('neotest').run.run(vim.fn.expand('%')<cr>",
+        { desc = 'Neotest test file' })
+      vim.keymap.set('n', '<leader>td', "<cmd>lua require('neotest').run.run({strategy = 'dap'})<cr>",
+        { desc = 'Neotest test debug' })
     end
   }
 }
---
+++ lua/plugins/nvim-dap.lua
@@ -0,0 +1,56 @@
+return {
+  {
+    'mfussenegger/nvim-dap',
+
+    dependencies = {
+      "rcarriga/nvim-dap-ui",
+      "theHamsta/nvim-dap-virtual-text",
+    },
+
+    config = function()
+      local dap = require('dap')
+
+      -- DOTNET
+      dap.adapters.netcoredbg = {
+        type = 'executable',
+        command = vim.fn.exepath("netcoredbg"),
+        args = { "--interpreter=vscode" },
+        options = { detached = false },
+      }
+
+      for _, lang in ipairs({ "cs", "fsharp", "vb" }) do
+        if not dap.configurations[lang] then
+          dap.configurations[lang] = {
+            {
+              type = "netcoredbg",
+              name = "Launch file",
+              request = "launch",
+              program = function()
+                return vim.fn.input("Path to dll: ", vim.fn.getcwd() .. "/", "file")
+              end,
+              cwd = "${workspaceFolder}",
+            },
+          }
+        end
+      end
+
+      -- KEYMAPS
+      vim.keymap.set('n', '<F5>', function() require('dap').continue() end, { desc = 'DAP continue' })
+      vim.keymap.set('n', '<Leader>dc', function() require('dap').continue() end, { desc = 'DAP continue' })
+      vim.keymap.set('n', '<Leader>dr', function() require('dap').run_to_cursor() end, { desc = 'DAP run to cursor' })
+      vim.keymap.set('n', '<F10>', function() require('dap').step_over() end, { desc = 'DAP step over' })
+      vim.keymap.set('n', '<Leader>do', function() require('dap').step_over() end, { desc = 'DAP step over' })
+      vim.keymap.set('n', '<F11>', function() require('dap').step_into() end, { desc = 'DAP step into' })
+      vim.keymap.set('n', '<Leader>di', function() require('dap').step_into() end, { desc = 'DAP step into' })
+      vim.keymap.set('n', '<F12>', function() require('dap').step_out() end, { desc = 'DAP step out' })
+      vim.keymap.set('n', '<Leader>dO', function() require('dap').step_out() end, { desc = 'DAP step out' })
+      vim.keymap.set('n', '<Leader>db', function() require('dap').toggle_breakpoint() end,
+        { desc = 'DAP toggle breakpoint' })
+      vim.keymap.set('n', '<Leader>dp',
+        function() require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end,
+        { desc = 'DAP log message breakpoint' })
+      vim.keymap.set('n', '<Leader>dr', function() require('dap').repl.open() end, { desc = 'DAP repl open' })
+      vim.keymap.set('n', '<Leader>dl', function() require('dap').run_last() end, { desc = 'DAP run last' })
+    end
+  }
+}
