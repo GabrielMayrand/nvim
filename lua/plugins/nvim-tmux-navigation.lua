aa26bf761ef70d5f046a434737c087c4994ac55f
GabrielMayrand <gabmayrand@hotmail.ca>
2025-01-18 19:16:32 -0500

    feat: added window resize shortcuts

--- lua/plugins/nvim-tmux-navigation.lua
+++ lua/plugins/nvim-tmux-navigation.lua
@@ -14,5 +14,15 @@ return {
       vim.keymap.set('n', "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
       vim.keymap.set('n', "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
       vim.keymap.set('n', "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
+
+      vim.keymap.set('n', "<M-h>", "<cmd>wincmd < <cr>")
+      vim.keymap.set('n', "<M-l>", "<cmd>wincmd > <cr>")
+      vim.keymap.set('n', "<M-j>", "<cmd>wincmd - <cr>")
+      vim.keymap.set('n', "<M-k>", "<cmd>wincmd + <cr>")
+
+      vim.keymap.set('n', "<M-H>", "<cmd>vertical resize -5<cr>")
+      vim.keymap.set('n', "<M-L>", "<cmd>vertical resize +5<cr>")
+      vim.keymap.set('n', "<M-J>", "<cmd>horizontal resize -5<cr>")
+      vim.keymap.set('n', "<M-K>", "<cmd>horizontal resize +5<cr>")
     end
   } }
