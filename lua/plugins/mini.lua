96d5b5b5329409e75fb58a348bb32735b240602d
Gabriel Mayrand <gabm@premiertech.com>
2025-03-16 05:05:06 -0400

    feat: mini utils

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,9 +1,18 @@
 return {
-    {
-        'echasnovski/mini.nvim',
-        config = function()
-            local statusline = require 'mini.statusline'
-            statusline.setup { use_icons = true }
-        end
-    }
+  {
+    'echasnovski/mini.nvim',
+    config = function()
+      local statusline = require 'mini.statusline'
+      statusline.setup { use_icons = true }
+
+      local surround = require 'mini.surround'
+      surround.setup {}
+
+      local pairs = require 'mini.pairs'
+      pairs.setup {}
+
+      local splitjoin = require 'mini.splitjoin'
+      splitjoin.setup {}
+    end
+  }
 }
